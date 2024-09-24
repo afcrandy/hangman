c2b2036c375d68bded59cfde2c62fbff0f4fca34
Andrew Reid <andrew.o.reid@gmail.com>
2024-09-24 18:15:50 +0000

    Add welcome screen

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from art import *
+
+def print_welcome():
+    """
+    Print the welcome screen at the beggining of the game
+    """
+    # insert ASCII art 'Hangman' to replace below
+    print(WELCOME_LOGO)
+    print('by Andrew Reid')
+    print()
+
+def main():
+    """
+    Run game loop
+    """
+    print('main was run')
+
+print_welcome()
+main()
